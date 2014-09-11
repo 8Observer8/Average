27b01d2a9461407f4bbf4b50271c89dca2795e0a
Enzhaev Ivan <8observer8@gmail.com>
2014-09-11 09:42:57 +0400

    First Commit

---
+++ Average.pro
@@ -0,0 +1,23 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2014-09-10T20:49:57
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = Average
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        Dialog.cpp
+
+HEADERS  += Dialog.h
+
+FORMS    += Dialog.ui
+
+RESOURCES += \
+    images.qrc
---
+++ Dialog.cpp
@@ -0,0 +1,37 @@
+
+#include <QMessageBox>
+#include "Dialog.h"
+#include "ui_Dialog.h"
+
+Dialog::Dialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+}
+
+Dialog::~Dialog()
+{
+    delete ui;
+}
+
+void Dialog::on_resultButton_clicked()
+{
+    bool ok;
+
+    double a = ui->aLineEdit->text().toDouble( &ok );
+    if ( !ok ) {
+        QMessageBox::information( this, tr( "Ошибка" ), tr( "Вы ввели некорректные данные" ) );
+        return;
+    }
+
+    double b = ui->bLineEdit->text().toDouble( &ok );
+    if ( !ok ) {
+        QMessageBox::information( this, tr( "Ошибка" ), tr( "Вы ввели некорректные данные" ) );
+        return;
+    }
+
+    double c = ( a + b ) / 2.0;
+
+    ui->cLineEdit->setText( QString::number( c ) );
+}
